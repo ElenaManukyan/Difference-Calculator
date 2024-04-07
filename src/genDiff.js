1c83835651783de0651a0b3b8d4d221ab5cc8fcf
SierraMoiseevna <utv.header@gmail.com>
2024-04-07 13:21:31 +0300

    Decrease Cognitive Complexity

--- src/genDiff.js
+++ src/genDiff.js
@@ -9,9 +9,7 @@ function genDiff(obj1, obj2) {
     const value2 = obj2[key];
     if (typeof value1 === 'object' && typeof value2 === 'object') {
       const nestedDiff = genDiff(value1, value2);
-      if (Object.keys(nestedDiff).length > 0) {
-        diff[key] = nestedDiff;
-      }
+      diff[key] = nestedDiff;
     } else if (value1 !== value2) {
       diff[`- ${key}`] = value1;
       diff[`+ ${key}`] = value2;
