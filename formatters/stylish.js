cb45ef88e0c14d2ca34c18e051167e3937904d7c
SierraMoiseevna <utv.header@gmail.com>
2024-04-09 17:27:31 +0300

    something

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -1,17 +1,19 @@
+import _ from 'lodash';
+
 const stylish = (obj, depth = 1) => {
   let i;
   let level = depth;
-  if (obj !== null) {
-  const keys = Object.keys(obj);
-  for (let j = 0; j < keys.length; j += 1) {
-    const key = keys[j];
-    i = obj[key];
-    if ((typeof i) === 'object') {
-      level += 1;
-      stylish(i);
+  if (_.isPlainObject(obj)) {
+    const keys = Object.keys(obj);
+    for (let j = 0; j < keys.length; j += 1) {
+      const key = keys[j];
+      i = obj[key];
+      if ((typeof i) === 'object') {
+        level += 1;
+        stylish(i);
+      }
     }
   }
-}
   return level;
 };
 
--- src/genDiff.js
+++ src/genDiff.js
@@ -2,106 +2,121 @@ import _ from 'lodash';
 import stylish from '../formatters/stylish.js';
 
 const object1 = {
-  "common": {
-    "setting1": "Value 1",
-    "setting2": 200,
-    "setting3": true,
-    "setting6": {
-      "key": "value",
-      "doge": {
-        "wow": ""
-      }
-    }
+  common: {
+    setting1: 'Value 1',
+    setting2: 200,
+    setting3: true,
+    setting6: {
+      key: 'value',
+      doge: {
+        wow: '',
+      },
+    },
   },
-  "group1": {
-    "baz": "bas",
-    "foo": "bar",
-    "nest": {
-      "key": "value"
-    }
+  group1: {
+    baz: 'bas',
+    foo: 'bar',
+    nest: {
+      key: 'value',
+    },
   },
-  "group2": {
-    "abc": 12345,
-    "deep": {
-      "id": 45
-    }
-  }
+  group2: {
+    abc: 12345,
+    deep: {
+      id: 45,
+    },
+  },
 };
 const object2 = {
-  "common": {
-    "follow": false,
-    "setting1": "Value 1",
-    "setting3": null,
-    "setting4": "blah blah",
-    "setting5": {
-      "key5": "value5"
+  common: {
+    follow: false,
+    setting1: 'Value 1',
+    setting3: null,
+    setting4: 'blah blah',
+    setting5: {
+      key5: 'value5',
     },
-    "setting6": {
-      "key": "value",
-      "ops": "vops",
-      "doge": {
-        "wow": "so much"
-      }
-    }
-  },
-  "group1": {
-    "foo": "bar",
-    "baz": "bars",
-    "nest": "str"
-  },
-  "group3": {
-    "deep": {
-      "id": {
-        "number": 45
-      }
+    setting6: {
+      key: 'value',
+      ops: 'vops',
+      doge: {
+        wow: 'so much',
+      },
     },
-    "fee": 100500
-  }
+  },
+  group1: {
+    foo: 'bar',
+    baz: 'bars',
+    nest: 'str',
+  },
+  group3: {
+    deep: {
+      id: {
+        number: 45,
+      },
+    },
+    fee: 100500,
+  },
 };
 
-let diff = [];
+const diff = [];
 // const item = { key: '', type: '', value: '' };
 function genDiff(obj1, obj2, formatName) {
-  // const diff = [];
-  // const item = { key: '', type: '', value: '' };
+  let level = 0;
+  let valueForPush = {};
   const keys1 = _.sortBy(Object.keys(obj1));
-  // const keys2 = Object.keys(obj2);
+  const keys2 = Object.keys(obj2);
   for (let keyIndex = 0; keyIndex < keys1.length; keyIndex += 1) {
     const key = keys1[keyIndex];
     const value1 = obj1[key];
     const value2 = obj2[key];
-    if (typeof value1 === 'object' && typeof value2 === 'object') {
+    if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
+      level += 1;
+      // console.log(`level = ${level}`);
       const nestedDiff = genDiff(value1, value2, formatName);
-      // diff = [];
-      diff.push({
-        key: key,
+
+      // console.log(`nestedDiff = ${JSON.stringify(nestedDiff, null, 4)}`);
+      valueForPush = {
+        key,
         type: 'nested',
-        value: [{ nestedDiff }],
-      });
-    } else if (value1 !== value2) {
-      diff.push({
-        key: key,
-        type: 'changed',
+        value: JSON.stringify(nestedDiff.slice(-level)), // JSON.stringify(value1),
+      };
+
+      diff.push(valueForPush);
+    } else if (!keys2.includes(key)) {
+      level += 1;
+      valueForPush = {
+        key,
+        type: 'removed',
         value: value1,
-        prevValue: value2,
-      });
-      //diff.push(item);
+      };
+      diff.push(valueForPush);
+    } else if (value1 !== value2) {
+      level += 1;
+      valueForPush = {
+        key,
+        type: 'changed',
+        value: value2,
+        prevValue: value1,
+      };
+      diff.push(valueForPush);
+      // diff.push(item);
       // diff[`- ${key}`] = value1;
       // diff[`+ ${key}`] = value2;
     } else if (value1 === value2) {
-      diff.push({
-        key: key,
+      level += 1;
+      valueForPush = {
+        key,
         type: 'unchanged',
         value: value1,
-      });
+      };
+      diff.push(valueForPush);
       // diff.push(item);
       // diff[`  ${key}`] = value1;
     }
   }
-  
 
-
-  const keys2 = Object.keys(obj2);
+  // const keys2 = _.sortBy(Object.keys(obj2));
   for (let keyIndex = 0; keyIndex < keys2.length; keyIndex += 1) {
     const key2 = keys2[keyIndex];
     // const value1 = obj1[key];
@@ -112,15 +127,23 @@ function genDiff(obj1, obj2, formatName) {
         type: 'added',
         value: value2,
       });
+    }
   }
-}
-
   return diff;
 }
 
+// console.log(genDiff(object1, object2, 'stylish'));
 console.log(genDiff(object1, object2, 'stylish'));
-console.log(JSON.stringify(genDiff(object1, object2, 'stylish')));
-// console.log(JSON.stringify(genDiff(object1, object2, 'stylish'), null, stylish(genDiff(object1, object2, 'stylish'))));
+/* const support = [];
+console.log(JSON.stringify(genDiff(object1, object2, 'stylish'), (key, value) => {
+  if (_.isPlainObject(value)) {
+    if (support.includes(value)) {
+      return;
+    }
+    support.push(value);
+  }
+  return value;
+})); */ // , stylish(genDiff(object1, object2, 'stylish'))));
 
 /* function genDiff(obj1, obj2, formatName) {
   const diff = {};
