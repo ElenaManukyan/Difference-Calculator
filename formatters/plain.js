8a4cc9963153998b862feaba44053787ed13ea19
SierraMoiseevna <utv.header@gmail.com>
2024-05-04 15:53:16 +0300

    Delete long comment from plain.js file

--- formatters/plain.js
+++ formatters/plain.js
@@ -31,30 +31,6 @@ function plain(diff, path = '', res = '') {
       }
     }
   });
-  /*
-  for (let i = 0; i < diff.length; i += 1) {
-    const element = diff[i];
-    const currentPath = `${path}${element.key}.`;
-    if (_.isArray(element.value)) {
-      result = plain(element.value, currentPath, result);
-    } else if (element.type === 'added') {
-      if (_.isPlainObject(element.value)) {
-        result += `Property '${currentPath.slice(0, -1)}' was added with value: [complex value]\n`;
-      } else {
-        result += `Property '${currentPath.slice(0, -1)}' was added with value: ${stringify(element.value)}\n`;
-      }
-    } else if (element.type === 'removed') {
-      result += `Property '${currentPath.slice(0, -1)}' was removed\n`;
-    } else if (element.type === 'changed') {
-      if (_.isPlainObject(element.value)) {
-        result += `Property '${currentPath.slice(0, -1)}' was updated. From ${element.prevValue} to [complex value]\n`;
-      } else if (_.isPlainObject(element.prevValue)) {
-        result += `Property '${currentPath.slice(0, -1)}' was updated. From [complex value] to ${stringify(element.value)}\n`;
-      } else {
-        result += `Property '${currentPath.slice(0, -1)}' was updated. From ${stringify(element.prevValue)} to ${stringify(element.value)}\n`;
-      }
-    }
-  } */
   return result;
 }
 
