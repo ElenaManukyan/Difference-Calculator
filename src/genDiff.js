9d9f6b251938874291d501514e78e0edca3e50f1
SierraMoiseevna <utv.header@gmail.com>
2024-05-04 15:59:53 +0300

    Delete long comment in genDiff.js file

--- src/genDiff.js
+++ src/genDiff.js
@@ -54,56 +54,6 @@ function genDiff(obj1, obj2, formatName) {
         _.sortBy(diff, 'key');
       }
     });
-    /*
-    for (let i = 0; i < unionKeys.length; i += 1) {
-      const key = unionKeys[i];
-      if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
-        valueForPush = {
-          key,
-          type: 'nested',
-          value: _.sortBy(genDiff(obj1[key], obj2[key], formatName), 'key'),
-        };
-        diff.push(valueForPush);
-      } else if (obj1[key] === obj2[key]) {
-        valueForPush = {
-          key,
-          type: 'unchanged',
-          value: obj1[key],
-        };
-        diff.push(valueForPush);
-        _.sortBy(diff, 'key');
-      } else if (
-        obj1[key] !== obj2[key]
-        && keys1.includes(key)
-        && keys2.includes(key)
-      ) {
-        valueForPush = {
-          key,
-          type: 'changed',
-          value: obj2[key],
-          prevValue: obj1[key],
-        };
-        diff.push(valueForPush);
-        _.sortBy(diff, 'key');
-      } else if (!keys1.includes(key) && keys2.includes(key)) {
-        valueForPush = {
-          key,
-          type: 'added',
-          value: obj2[key],
-        };
-        diff.push(valueForPush);
-        _.sortBy(diff, 'key');
-      } else {
-        valueForPush = {
-          key,
-          type: 'removed',
-          value: obj1[key],
-        };
-        diff.push(valueForPush);
-        _.sortBy(diff, 'key');
-      }
-    }
-    */
     _.sortBy(diff, 'key');
   }
   return _.sortBy(diff, 'key');
