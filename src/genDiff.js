40834139520aa20ede6c50f6c3ffbaa8220ea17c
SierraMoiseevna <utv.header@gmail.com>
2024-05-05 21:32:16 +0300

    refactoring genDiff.js file

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,62 +1,49 @@
 import _ from 'lodash';
 
 function genDiff(obj1, obj2, formatName) {
-  let valueForPush;
-  const diff = [];
   if (_.isPlainObject(obj1) && _.isPlainObject(obj2)) {
     const keys1 = _.sortBy(Object.keys(obj1));
     const keys2 = _.sortBy(Object.keys(obj2));
     const unionKeys = _.union(keys1, keys2);
-    unionKeys.forEach((key) => {
+    return _.sortBy(unionKeys.map((key) => {
       if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
-        valueForPush = {
+        return {
           key,
           type: 'nested',
           value: _.sortBy(genDiff(obj1[key], obj2[key], formatName), 'key'),
         };
-        diff.push(valueForPush);
-      } else if (obj1[key] === obj2[key]) {
-        valueForPush = {
+      } if (obj1[key] === obj2[key]) {
+        return {
           key,
           type: 'unchanged',
           value: obj1[key],
         };
-        diff.push(valueForPush);
-        _.sortBy(diff, 'key');
-      } else if (
+      } if (
         obj1[key] !== obj2[key]
         && keys1.includes(key)
         && keys2.includes(key)
       ) {
-        valueForPush = {
+        return {
           key,
           type: 'changed',
           value: obj2[key],
           prevValue: obj1[key],
         };
-        diff.push(valueForPush);
-        _.sortBy(diff, 'key');
-      } else if (!keys1.includes(key) && keys2.includes(key)) {
-        valueForPush = {
+      } if (!keys1.includes(key) && keys2.includes(key)) {
+        return {
           key,
           type: 'added',
           value: obj2[key],
         };
-        diff.push(valueForPush);
-        _.sortBy(diff, 'key');
-      } else {
-        valueForPush = {
-          key,
-          type: 'removed',
-          value: obj1[key],
-        };
-        diff.push(valueForPush);
-        _.sortBy(diff, 'key');
       }
-    });
-    _.sortBy(diff, 'key');
+      return {
+        key,
+        type: 'removed',
+        value: obj1[key],
+      };
+    }), 'key');
   }
-  return _.sortBy(diff, 'key');
+  return [];
 }
 
 export default genDiff;
