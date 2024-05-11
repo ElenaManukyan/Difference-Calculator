5e2ba8bdef404dc2f31f64c94308153669146717
SierraMoiseevna <utv.header@gmail.com>
2024-05-11 16:14:27 +0300

    change stylish.js file

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -39,7 +39,8 @@ function formatterForStylish(object) {
 }
 
 function stylish(object) {
-  const resJsonString = JSON.stringify(stylishObj(object), null, formatterForStylish(object));
+  // const resJsonString = JSON.stringify(stylishObj(object), null, formatterForStylish(object));
+  const resJsonString = JSON.stringify(stylishObj(object), null, 1);
   const newJsonString = resJsonString.replace(/"/g, '');
   const result = newJsonString.replace(/,/g, '');
   return result;
