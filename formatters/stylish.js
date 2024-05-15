7a310a1b04a535345f7ed164c4682c15681c65cc
SierraMoiseevna <utv.header@gmail.com>
2024-05-15 18:27:28 +0300

    little changes

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -10,17 +10,8 @@ function stylish(list) {
         res += innerFunc(element.value, levelOfDepth + 1);
         res += `${countedSpaces}}\n`;
       } else {
-        let value;
-        let prevValue;
-        value = typeof element.value === 'object' ? JSON.stringify(element.value, null, countedSpaces).replace(/"/g, '').replace(/,/g, '') : element.value;
-        prevValue = typeof element.prevValue === 'object' ? JSON.stringify(element.prevValue, null, countedSpaces).replace(/"/g, '').replace(/,/g, '') : element.prevValue;
-        /*if (typeof element.value === 'object' || typeof element.prevValue === 'object') {
-          value = JSON.stringify(element.value, null, countedSpaces).replace(/"/g, '').replace(/,/g, '');
-          prevValue = JSON.stringify(element.prevValue, null, countedSpaces).replace(/"/g, '').replace(/,/g, '');
-        } else {
-          value = element.value;
-          prevValue = element.prevValue;
-        }*/
+        const value = typeof element.value === 'object' ? JSON.stringify(element.value, null, countedSpaces).replace(/"/g, '').replace(/,/g, '') : element.value;
+        const prevValue = typeof element.prevValue === 'object' ? JSON.stringify(element.prevValue, null, countedSpaces).replace(/"/g, '').replace(/,/g, '') : element.prevValue;
         switch (element.type) {
           case 'added':
             res += `${countedSpaces.slice(0, -2)}+ ${element.key}: ${value}\n`;
@@ -43,7 +34,7 @@ function stylish(list) {
     return res;
   }
   const result = innerFunc(list, 1);
-  return result;
+  return result.slice(0, -1);
 }
 
 
