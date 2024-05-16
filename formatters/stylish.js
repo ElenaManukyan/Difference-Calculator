62526cda8d55ebd71fcb5a9b5c7e7eff9faa25f9
SierraMoiseevna <utv.header@gmail.com>
2024-05-16 19:11:24 +0300

    refactoring stylish.js code

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -1,48 +1,52 @@
 import _ from 'lodash';
 
+function stringifyValue(value, levelOfDepth) {
+  if (_.isPlainObject(value)) {
+    const indent = ' '.repeat(4 * (levelOfDepth + 1));
+    const closingIndent = ' '.repeat(4 * levelOfDepth);
+    const entries = Object.entries(value).map(([key, val]) => {
+      const formattedValue = _.isPlainObject(val) ? stringifyValue(val, levelOfDepth + 1) : val;
+      return `${indent}${key}: ${formattedValue}`;
+    }).join('\n');
+    return `{\n${entries}\n${closingIndent}}`;
+  }
+  return value;
+}
+
 function stylish(list) {
   function innerFunc(listOfDifference, levelOfDepth) {
     let res = '';
     const countedSpaces = ' '.repeat(4 * levelOfDepth);
     listOfDifference.forEach((element) => {
-      if (_.isArray(element.value)) {
-        res += `${countedSpaces}${element.key}: {\n`;
-        res += innerFunc(element.value, levelOfDepth + 1);
-        res += `${countedSpaces}}\n`;
-      } else {
-        let { value } = element;
-        let { prevValue } = element;
-        if (_.isPlainObject(element.value)) {
-          value = JSON.stringify(element.value, null, 4 * (levelOfDepth + 1)).replace(/"/g, '').replace(/,/g, '');
-        }
-        if (_.isPlainObject(element.prevValue)) {
-          prevValue = JSON.stringify(element.prevValue, null, 4 * (levelOfDepth + 1)).replace(/"/g, '').replace(/,/g, '');
-        }
-        switch (element.type) {
-          case 'added':
-            res += `${countedSpaces.slice(0, -2)}+ ${element.key}: ${value}\n`;
-            break;
-          case 'unchanged':
-            res += `${countedSpaces.slice(0, -2)}  ${element.key}: ${value}\n`;
-            break;
-          case 'removed':
-            res += `${countedSpaces.slice(0, -2)}- ${element.key}: ${value}\n`;
-            break;
-          case 'changed':
-            res += `${countedSpaces.slice(0, -2)}- ${element.key}: ${prevValue}\n`;
-            res += `${countedSpaces.slice(0, -2)}+ ${element.key}: ${value}\n`;
-            break;
-          default:
-            throw new Error(`Element type ${element.type} doesn't exist`);
-        }
+      switch (element.type) {
+        case 'nested':
+          res += `${countedSpaces}${element.key}: {\n`;
+          res += `${innerFunc(stringifyValue(element.value, levelOfDepth), levelOfDepth + 1)}`;
+          res += `${countedSpaces}}\n`;
+          break;
+        case 'added':
+          res += `${countedSpaces.slice(0, -2)}+ ${element.key}: ${stringifyValue(element.value, levelOfDepth)}\n`;
+          break;
+        case 'unchanged':
+          res += `${countedSpaces.slice(0, -2)}  ${element.key}: ${stringifyValue(element.value, levelOfDepth)}\n`;
+          break;
+        case 'removed':
+          res += `${countedSpaces.slice(0, -2)}- ${element.key}: ${stringifyValue(element.value, levelOfDepth)}\n`;
+          break;
+        case 'changed':
+          res += `${countedSpaces.slice(0, -2)}- ${element.key}: ${stringifyValue(element.prevValue, levelOfDepth)}\n`;
+          res += `${countedSpaces.slice(0, -2)}+ ${element.key}: ${stringifyValue(element.value, levelOfDepth)}\n`;
+          break;
+        default:
+          throw new Error(`Element type ${element.type} doesn't exist`);
       }
     });
     return res;
   }
-  const result = innerFunc(list, 1);
-  return result.slice(0, -1);
+  let result = '';
+  result += innerFunc(list, 1);
+  return `${result.slice(0, -1)}\n}`;
 }
-
 
 /*
 function stylishObj(list) {
