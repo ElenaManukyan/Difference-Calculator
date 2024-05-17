e7ddbb8234d6e3c327e597eded396d00af6261c4
SierraMoiseevna <utv.header@gmail.com>
2024-05-17 15:58:46 +0300

    remake plain.js file

--- formatters/plain.js
+++ formatters/plain.js
@@ -7,7 +7,38 @@ function stringify(value) {
   return String(value);
 }
 
-function plain(diff, path = '', res = '') {
+function plain(diff, path = '') {
+  const result = diff.map((element) => {
+    const currentPath = `${path}${element.key}.`;
+    switch (element.type) {
+      case 'nested':
+        return plain(element.value, currentPath);
+      case 'added':
+        if (_.isPlainObject(element.value)) {
+          return `Property '${currentPath.slice(0, -1)}' was added with value: [complex value]\n`;
+        }
+        return `Property '${currentPath.slice(0, -1)}' was added with value: ${stringify(element.value)}\n`;
+
+      case 'removed':
+        return `Property '${currentPath.slice(0, -1)}' was removed\n`;
+      case 'changed':
+        if (_.isPlainObject(element.value)) {
+          return `Property '${currentPath.slice(0, -1)}' was updated. From ${element.prevValue} to [complex value]\n`;
+        }
+        if (_.isPlainObject(element.prevValue)) {
+          return `Property '${currentPath.slice(0, -1)}' was updated. From [complex value] to ${stringify(element.value)}\n`;
+        }
+        return `Property '${currentPath.slice(0, -1)}' was updated. From ${stringify(element.prevValue)} to ${stringify(element.value)}\n`;
+      case 'unchanged':
+        // return `Property '${currentPath.slice(0, -1)}' was unchanged.`;
+        return '';
+      default:
+        throw new Error(`Element type ${element.type} doesn't exist`);
+    }
+  });
+  return result.join('');
+}
+/*
   let result = res;
   diff.forEach((element) => {
     const currentPath = `${path}${element.key}.`;
@@ -31,7 +62,8 @@ function plain(diff, path = '', res = '') {
       }
     }
   });
-  return result;
-}
+  */
+// return result;
+// }
 
 export default plain;
