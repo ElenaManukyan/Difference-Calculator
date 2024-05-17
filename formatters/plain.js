ddab0d2e468be6a98666db9e93f0e60b9ce3d78a
SierraMoiseevna <utv.header@gmail.com>
2024-05-17 18:40:44 +0300

    add additional function to plain.js file

--- formatters/plain.js
+++ formatters/plain.js
@@ -7,34 +7,35 @@ function stringify(value) {
   return String(value);
 }
 
+function additionalFunction(difference, pathDepth = '') {
+  const result = difference.map((element) => {
+    const currentPath = `${pathDepth}${element.key}.`;
+    switch (element.type) {
+      case 'nested':
+        return additionalFunction(element.value, currentPath);
+      case 'added':
+        return _.isPlainObject(element.value) ? `Property '${currentPath.slice(0, -1)}' was added with value: [complex value]\n` : `Property '${currentPath.slice(0, -1)}' was added with value: ${stringify(element.value)}\n`;
+      case 'removed':
+        return `Property '${currentPath.slice(0, -1)}' was removed\n`;
+      case 'changed':
+        if (_.isPlainObject(element.value)) {
+          return `Property '${currentPath.slice(0, -1)}' was updated. From ${element.prevValue} to [complex value]\n`;
+        }
+        if (_.isPlainObject(element.prevValue)) {
+          return `Property '${currentPath.slice(0, -1)}' was updated. From [complex value] to ${stringify(element.value)}\n`;
+        }
+        return `Property '${currentPath.slice(0, -1)}' was updated. From ${stringify(element.prevValue)} to ${stringify(element.value)}\n`;
+      case 'unchanged':
+        return '';
+      default:
+        throw new Error(`Element type ${element.type} doesn't exist`);
+    }
+  });
+  return result.join('');
+}
+
 function plain(diff, path = '') {
-  function innerFunction(difference, pathDepth = '') {
-    const result = difference.map((element) => {
-      const currentPath = `${pathDepth}${element.key}.`;
-      switch (element.type) {
-        case 'nested':
-          return innerFunction(element.value, currentPath);
-        case 'added':
-          return _.isPlainObject(element.value) ? `Property '${currentPath.slice(0, -1)}' was added with value: [complex value]\n` : `Property '${currentPath.slice(0, -1)}' was added with value: ${stringify(element.value)}\n`;
-        case 'removed':
-          return `Property '${currentPath.slice(0, -1)}' was removed\n`;
-        case 'changed':
-          if (_.isPlainObject(element.value)) {
-            return `Property '${currentPath.slice(0, -1)}' was updated. From ${element.prevValue} to [complex value]\n`;
-          }
-          if (_.isPlainObject(element.prevValue)) {
-            return `Property '${currentPath.slice(0, -1)}' was updated. From [complex value] to ${stringify(element.value)}\n`;
-          }
-          return `Property '${currentPath.slice(0, -1)}' was updated. From ${stringify(element.prevValue)} to ${stringify(element.value)}\n`;
-        case 'unchanged':
-          return '';
-        default:
-          throw new Error(`Element type ${element.type} doesn't exist`);
-      }
-    });
-    return result.join('');
-  }
-  const result = innerFunction(diff, path);
+  const result = additionalFunction(diff, path);
   return result.slice(0, -1);
 }
 
