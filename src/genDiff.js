ad97fe2add683395673f37931a9041ca2500be14
SierraMoiseevna <utv.header@gmail.com>
2024-05-17 19:53:32 +0300

    refactoring code

--- src/genDiff.js
+++ src/genDiff.js
@@ -36,43 +36,6 @@ function genDiff(obj1, obj2) {
         return makeObj(key, 'added', obj2[key]);
       }
       return makeObj(key, 'removed', obj1[key]);
-      /*
-      if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
-        return {
-          key,
-          type: 'nested',
-          value: _.sortBy(genDiff(obj1[key], obj2[key]), 'key'),
-        };
-      } if (obj1[key] === obj2[key]) {
-        return {
-          key,
-          type: 'unchanged',
-          value: obj1[key],
-        };
-      } if (
-        obj1[key] !== obj2[key]
-        && keys1.includes(key)
-        && keys2.includes(key)
-      ) {
-        return {
-          key,
-          type: 'changed',
-          value: obj2[key],
-          prevValue: obj1[key],
-        };
-      } if (!keys1.includes(key) && keys2.includes(key)) {
-        return {
-          key,
-          type: 'added',
-          value: obj2[key],
-        };
-      }
-      return {
-        key,
-        type: 'removed',
-        value: obj1[key],
-      };
-      */
     }), 'key');
   }
   return [];
