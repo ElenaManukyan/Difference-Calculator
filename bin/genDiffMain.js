e25de9b5fada7c69ed9a27f6cfc4758825780908
SierraMoiseevna <utv.header@gmail.com>
2024-05-22 20:40:09 +0300

    refactoring

--- bin/genDiffMain.js
+++ bin/genDiffMain.js
@@ -10,9 +10,9 @@ program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <format>', 'output format')
+  .option('-f, --format <format>', 'output format', 'stylish')
   .action((filepath1, filepath2, options) => {
-    const { format } = options || 'stylish';
+    const { format } = options;
     console.log(parsePaths(filepath1, filepath2, format));
   });
 
