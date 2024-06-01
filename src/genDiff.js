6a672808180517544467a40f9a8f0884a59d1984
SierraMoiseevna <utv.header@gmail.com>
2024-06-01 13:58:27 +0300

    remove function genDiff

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,21 +1,5 @@
 import _ from 'lodash';
 
-function makeObj(key, typeVal, valueVal, prevValueVal = '&') {
-  if (prevValueVal !== '&') {
-    return {
-      key,
-      type: typeVal,
-      value: valueVal,
-      prevValue: prevValueVal,
-    };
-  }
-  return {
-    key,
-    type: typeVal,
-    value: valueVal,
-  };
-}
-
 function genDiff(obj1, obj2) {
   if (_.isPlainObject(obj1) && _.isPlainObject(obj2)) {
     const keys1 = _.sortBy(Object.keys(obj1));
@@ -23,18 +7,39 @@ function genDiff(obj1, obj2) {
     const unionKeys = _.union(keys1, keys2);
     return _.sortBy(unionKeys.map((key) => {
       if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
-        return makeObj(key, 'nested', genDiff(obj1[key], obj2[key]));
+        return {
+          key,
+          type: 'nested',
+          value: genDiff(obj1[key], obj2[key]),
+        };
       }
       if (!keys1.includes(key) && keys2.includes(key)) {
-        return makeObj(key, 'added', obj2[key]);
+        return {
+          key,
+          type: 'added',
+          value: obj2[key],
+        };
       }
       if (keys1.includes(key) && !keys2.includes(key)) {
-        return makeObj(key, 'removed', obj1[key]);
+        return {
+          key,
+          type: 'removed',
+          value: obj1[key],
+        };
       }
       if (obj1[key] === obj2[key]) {
-        return makeObj(key, 'unchanged', obj1[key]);
+        return {
+          key,
+          type: 'unchanged',
+          value: obj1[key],
+        };
       }
-      return makeObj(key, 'changed', obj2[key], obj1[key]);
+      return {
+        key,
+        type: 'changed',
+        value: obj2[key],
+        prevValue: obj1[key],
+      };
     }), 'key');
   }
   return [];
